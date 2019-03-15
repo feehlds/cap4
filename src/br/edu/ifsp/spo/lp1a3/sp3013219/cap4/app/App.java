2d6f6cffae711cb1f960ceb95269fb3ec36e533a
Felipe Loureiro <felipe-loureiro@outlook.com>
2019-03-15 16:52:30 -0300

    Commit 3

--- src/br/edu/ifsp/spo/lp1a3/sp3013219/cap4/app/App.java
+++ src/br/edu/ifsp/spo/lp1a3/sp3013219/cap4/app/App.java
@@ -1,15 +1,32 @@
 package br.edu.ifsp.spo.lp1a3.sp3013219.cap4.app;
 import java.util.Scanner;
 import br.edu.ifsp.spo.lp1a3.sp3013219.cap4.cripto.*;
+import br.edu.ifsp.spo.lp1a3.sp3013219.cap4.descripto.Decrypt;
 
 public class App {
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
 		int num;
+		
 		Cripto crip = new Cripto();
-			
-		System.out.println("Digite 4 digitos sem espaço: ");
-		num = ler.nextInt();
-		System.out.println("Número criptografado: " + crip.criptografar(num));
+		Decrypt decrip  = new Decrypt();	
+		
+		
+		System.out.println("Digite 1 Para Criptografar um número.");
+		System.out.println("Digite 2 Para Descriptografar um número.");
+		int op = ler.nextInt();
+		
+		switch(op) {
+			case 1 :
+				System.out.println("Digite 4 digitos sem espaços: ");
+				num = ler.nextInt();
+				System.out.println("Número criptografado: " + crip.criptografar(num));
+			break;
+			case 2 :
+				System.out.println("Digite 4 digitos sem espaços: ");
+				num = ler.nextInt();
+				System.out.println("Número descriptografado: " + decrip.decrypt(num));
+			break;
 		}			
+	}
 }
--- src/br/edu/ifsp/spo/lp1a3/sp3013219/cap4/descripto/Decrypt.java
+++ src/br/edu/ifsp/spo/lp1a3/sp3013219/cap4/descripto/Decrypt.java
@@ -10,25 +10,25 @@ public class Decrypt {
 		for(i = 0; i<4; i++) {
 			if(i == 0  || i == 2) {
 				x = (int) (10 * Math.pow(10, i));
-				if((num / y + 10) < 11) {
-					drcryptNum = drcryptNum + ((num / y + 10) - 7) * x;
-					} else {
-						drcryptNum = drcryptNum + ((num / y + 10) - 7 - 10) * x;
-					}
+				
+					drcryptNum = drcryptNum + ((num / y - 7) + 10) * x;
+					
+						//drcryptNum = drcryptNum + (num / y - 7) * x;
+					
 				num = (int) num % y;
 				y = (int) y / 10;
 			}
 			else{
 				x = (int) (1 * Math.pow(10, i - 1));
-				if((num / y + 10) < 11) {
-				drcryptNum = drcryptNum + ((num / y + 10) - 7) * x;
-				} else {
-					drcryptNum = drcryptNum + ((num / y + 10) - 7 - 10) * x;
+				
+				drcryptNum = drcryptNum + ((num / y - 7) + 10) * x;
+			
+					//drcryptNum = drcryptNum + (num / y - 7) * x;
 				}
 				num = (int) num % y;
 				y = (int) y / 10;
 				}		
-		}
+		
 		return drcryptNum;
 	}
 }
